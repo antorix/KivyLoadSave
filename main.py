fe2fab27884c2ac4ca5430fcfe7bd4c7b60c4cc3
rocketstudios <inoblogger@gmail.com>
2023-11-23 16:13:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class KivyLoadSave(App):
         self.box = BoxLayout(orientation="vertical")  # create our simple interface
         self.button1 = Button(text="Click here to LOAD any text file\nfrom your device memory",
                               halign="center", size_hint_y=.2)
-        self.button1.bind(on_release=self.choose_file)
+        self.button1.bind(on_release=lambda x: self.chooser.choose_content("text/*"))
         self.box.add_widget(self.button1)
         self.input = TextInput(hint_text="You will see loaded text here. Or type your own text to save it.",
                                input_type="text", background_color="white", background_normal = "")
@@ -43,21 +43,17 @@ class KivyLoadSave(App):
 
         return self.box
 
-    def choose_file(self, instance=None):
-        """ Launch Android chooser dialog to choose a file """
-        self.chooser.choose_content("text/*")        
-
     def chooser_callback(self, uri_list):
         """ Callback handling the chooser """
         try:
             for uri in uri_list:
                 # We obtain the file from the Android's "Shared storage", but we can't work with it directly.
                 # We need to first copy it to our apps "Private storage." Then our 'opened_file' variable
-                # gets the path of this copy:
+                # receives the path of the copied file:
                 self.opened_file = SharedStorage().copy_from_shared(uri)
 
-            # This looks a bit crazy, but this is the way to quit the callback and invoke 'load_file'.
-            # It won't work if we launch it from here directly:
+            # The Clock approach below looks a bit crazy, but it's the way to invoke 'load_file'
+            # outside of the callback (otherwise it won't work):
             Clock.schedule_interval(self.load_file, .05)
 
         except Exception as e:
@@ -68,7 +64,6 @@ class KivyLoadSave(App):
         if self.opened_file is not None:
             with open(self.opened_file, "r") as file:  # now we can read the obtained path in a normal Python way
                 self.input.text = str(file.read()).strip()
-                #self.input.text = str(file.read()).strip()
 
             Popup(
                 title="Info",
@@ -78,30 +73,27 @@ class KivyLoadSave(App):
                 size_hint=self.popup_size_hint
             ).open()
 
-            self.opened_file = None  # reverting back to None
+            self.opened_file = None  # reverting path back to None
 
         Clock.unschedule(self.load_file)  # stop scheduled action in the end
 
     def save_file(self, instance):
         """ Save the content of the self.input to device's Documents folder """
         filename = os.path.join(SharedStorage().get_cache_dir(),
-                                "My text from KivyLoadSave.txt")  # form the path of our new file
+                                "My text from KivyLoadSave.txt")  # forming the path of our new file
 
         with open(filename, "w") as file:  # now we can create it in a normal way, but only in the private storage
             file.write(self.input.text.strip())
 
-        result = SharedStorage().copy_to_shared(private_file=filename)  # but then we can copy it to the shared storage
+        SharedStorage().copy_to_shared(private_file=filename)  # but then we can copy it to the shared storage
 
-        # Now let's create an empty .mp3 file with a similar name. See the end of the code why.
-
-        if result:
-            Popup(
-                title="Info",
-                content=Label(
-                    text=f"File successfully saved to\nyour Documents, check it there!"
-                ),
-                size_hint=self.popup_size_hint
-            ).open()  # hurray!
+        Popup(
+            title="Info",
+            content=Label(
+                text=f"File successfully saved to\nyour Documents, check it there!"
+            ),
+            size_hint=self.popup_size_hint
+        ).open()  # hurray!
 
         # Note that Android copies our file to the Documents folder. It was done automatically. Why?
         # Because we created a .txt file which Android recognizes as a document. If we create an .mp3 file,
@@ -117,6 +109,9 @@ class KivyLoadSave(App):
         temp = SharedStorage().get_cache_dir()  # clean our temporary file storage
         if temp and os.path.exists(temp):
             shutil.rmtree(temp)
+
+        # Read https://github.com/Android-for-Python/androidstorage4kivy
+        # for more technical details on this.
 
 
 if __name__ == "__main__":
